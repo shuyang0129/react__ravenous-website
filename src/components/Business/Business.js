84f35dc88b2ccd4dda3e784e4d48ab2f12a3d26d
Shuyang <shuyang0129@gmail.com>
2018-06-25 19:24:13 +0800

    Export the Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -14,26 +14,28 @@ const business = {
 }
 
 class Business extends React.Component {
- render() {
-     return (
-        <div className="Business">
-            <div className="image-container">
-                <img src={business.imageSrc} alt="" />
-            </div>
-            <h2>{business.name}</h2>
-            <div className="Business-information">
-                <div className="Business-address">
-                    <p>{business.address}</p>
-                    <p>{business.city}</p>
-                    <p>{business.state + " " + business.zipCode}</p>
+    render() {
+        return (
+            <div className="Business">
+                <div className="image-container">
+                    <img src={business.imageSrc} alt="" />
                 </div>
-                <div className="Business-review">
-                    <h3>{business.category}</h3>
-                    <h3 className="rating">{business.rating + " starts"}</h3>
-                    <p>{business.reviewCount + " reviews"}</p>
+                <h2>{business.name}</h2>
+                <div className="Business-information">
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state + " " + business.zipCode}</p>
+                    </div>
+                    <div className="Business-review">
+                        <h3>{business.category}</h3>
+                        <h3 className="rating">{business.rating + " starts"}</h3>
+                        <p>{business.reviewCount + " reviews"}</p>
+                    </div>
                 </div>
-            </div>
-        </div>      
-     );
- }
+            </div>      
+        );
+    }
 }
+
+export default Business;
