91850dffd3145d62d351577126d95ef82d06a2ca
Shuyang <shuyang0129@gmail.com>
2018-06-29 09:34:00 +0800

    Get a sort option's class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,6 +17,14 @@ class SearchBar extends React.Component {
         'Most Reviewed': 'review_count'
     }
 
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === this.sortByOption) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
