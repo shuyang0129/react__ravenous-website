10510f6a3532b182d080b98a3c33c4f1b7c53b1b
Shuyang <shuyang0129@gmail.com>
2018-07-09 23:21:59 +0800

    Fix bugs

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,15 +1,15 @@
 const apiKey ='l4zJw4ZYREuyUlGtcXr4Cp8rXV0vAUplbO_SW_EUyP9UBa6DiRql6IpaZiTiTqSD-MhZEfDQFuTsDvMvpoVogebjuj3EM9ZLoFZ7WnTnCrJOA3v4-pNoAxAGEOVCW3Yx';
 const Yelp = {
     search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by${sortBy}`, {
-            header: {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
                 Authorization: `Bearer ${apiKey}`
             }
-        }).then(response => response.json())
-        .then(jsonResponse => {
-            if(jsonResponse.business) {
-                jsonResponse.business.map(business => {
-                    const business = {
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
                         id: business.id,
                         imageSrc: business.image_url,
                         name: business.name,
@@ -17,12 +17,10 @@ const Yelp = {
                         city: business.location.city,
                         state: business.location.state,
                         zipCode: business.location.zip_code,
-                        category: business.category.title,
+                        category: business.categories[0].title,
                         rating: business.rating,
                         reviewCount: business.review_count
-                    };
-                    return business;
-                });
+                }));
             }
         });
     }
